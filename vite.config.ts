4d791dd2ecc1f0db6ec7f413430b679953c7c196
Kazuma Sakaguchi <sakaguchi@SRC-DPC-MAC-005.local>
2024-09-26 12:48:04 +0900

    vite-plugin-pwa導入

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      srcDir:"/",
+      manifest: {
+        name: "PWA App",
+        short_name: "PWA App",
+        description: "PWA Test App",
+        lang: "ja",
+        display: "standalone"
+      }
+    })
+  ],
   server: {
     host: true,
     port: 5173
