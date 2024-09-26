e3a6af2ba80413f237a9a3d6a19a18af3dfd25e1
Kazuma Sakaguchi <sakaguchi@SRC-DPC-MAC-005.local>
2024-09-26 12:28:25 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted } from 'vue';
+
+const isPWA = ref(false);
+const checkIfPWA = () => {
+  isPWA.value =
+    window.matchMedia('(display-mode: standalone)').matches ||
+    (window.navigator as any).standalone === true ||
+    document.referrer.includes('android-app://');
+};
+
+onMounted(() => {
+  checkIfPWA();
+});
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+  <div>
+    <p v-if="isPWA">PWAとして閲覧しています。</p>
+    <p v-else>ブラウザで閲覧しています。</p>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
