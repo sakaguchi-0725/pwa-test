430c877960252a134e8d5d9af4b7f1d822d4539d
Kazuma Sakaguchi <sakaguchi@SRC-DPC-MAC-005.local>
2024-11-21 15:00:56 +0900

    registerSW

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import './style.css'
 import App from './App.vue'
 import { router } from './router'
 import Vue3TouchEvents, { Vue3TouchEventsOptions } from 'vue3-touch-events'
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({ immediate: true })
 
 const app = createApp(App)
 app.use<Vue3TouchEventsOptions>(Vue3TouchEvents, {})
